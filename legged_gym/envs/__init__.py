50954e4d31047c62f9f5e8f938287d96f502701a
Lain-Ego <2302109068@qq.com>
2026-02-11 10:10:06 +0800

    添加Mujoco迁移部分

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -29,7 +29,6 @@
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
-from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
 from .anymal_c.anymal import Anymal
 from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
@@ -39,9 +38,14 @@ from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 
+# ===================================================================
+# [新增] 导入 HTDW-4438 的配置类
+# 假设你的文件路径是 legged_gym/envs/htdw_4438/htdw_4438_config.py
+# ===================================================================
+from .htdw_4438.htdw_4438_config import Htdw4438Cfg, Htdw4438CfgPPO
+
 
 import os
-
 from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
@@ -49,3 +53,13 @@ task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCf
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+
+# ===================================================================
+# [新增] 注册 HTDW-4438 任务
+# ===================================================================
+task_registry.register( 
+    "htdw_4438",            # 任务名称 (训练时用 --task htdw_4438)
+    LeggedRobot,            # 机器人基类 (使用标准的 LeggedRobot)
+    Htdw4438Cfg(),          # 环境配置实例
+    Htdw4438CfgPPO()        # 训练配置实例
+)
