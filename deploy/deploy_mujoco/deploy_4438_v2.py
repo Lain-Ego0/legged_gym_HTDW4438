043b6d55943fc926000bf452c5f040c2e35e381f
Lain-Ego <2302109068@qq.com>
2026-02-26 17:41:49 +0800

    Update deploy_4438_v2.py
    仅修改测试

--- deploy/deploy_mujoco/deploy_4438_v2.py
+++ deploy/deploy_mujoco/deploy_4438_v2.py
@@ -20,7 +20,7 @@ class Cfg:
     MESHES_DIR = os.path.join(ROBOT_DIR, "meshes")
 
     YAML_PATH = os.path.join(PROJECT_ROOT, "deploy/deploy_mujoco/config/htdw_4438.yaml")
-    ONNX_PATH = os.path.join(PROJECT_ROOT, "onnx/policy_1500.onnx")
+    ONNX_PATH = os.path.join(PROJECT_ROOT, "onnx/htdw_4438_standard_20260226_165638_model_1500.onnx")
 
     sim_dt = 0.005              # 200Hz 物理步长
     decimation = 4              # 50Hz 策略频率
